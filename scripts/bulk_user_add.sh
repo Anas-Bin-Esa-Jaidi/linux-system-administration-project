50224bfb2fdbabf60794c028ecbb55537cb3b043
Anas-Bin-Esa-Jaidi <anasanas51808@gmail.com>
2026-04-11 12:57:06 +0530

    Update bulk_user_add.sh

--- scripts/bulk_user_add.sh
+++ scripts/bulk_user_add.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+GROUP="students"
+PASSWORD="Welcome@123"
+
+groupadd -f $GROUP
+
+for USER in $(cat user_list.txt)
+do
+useradd -m -g $GROUP $USER
+echo "$USER:$PASSWORD" | chpasswd
+done
+
+echo "Users added successfully."
+
+
+
